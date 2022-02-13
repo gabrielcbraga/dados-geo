3d4f22c11ec361615fdc57a4d79aa0d4c8bd51c5
Gabriel Braga <gabrielbraga0912@gmail.com>
2022-02-12 22:38:54 -0300

    separator created

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -6,18 +6,21 @@ import ImageHome from '../images/homepage.png'
 
 const Main = () => {
     return (
-        <div className="main">
-            <div className="titles-main">
-                <h3>Seu portal de geoprocessamento.</h3>
-                <h6>Conjunto de links de sites com dados georreferenciados, feito para te ajudar!</h6>
-            <div className="button-main">
-                <a href="#links">Veja os links</a>
+        <>
+            <div className="main">
+                <div className="titles-main">
+                    <h3>Seu portal de geoprocessamento.</h3>
+                    <h6>Conjunto de links de sites com dados georreferenciados, feito para te ajudar!</h6>
+                    <div className="button-main">
+                        <a href="#links">Veja os links</a>
+                    </div>
+                </div>
+                <div className="image-main">
+                    <img src={ImageHome} alt="" />
+                </div>
             </div>
-            </div>
-            <div className="image-main">
-                <img src={ImageHome} alt="" />
-            </div>
-        </div>
+            <div className="main-separator"></div>
+        </>
     )
 }
 
